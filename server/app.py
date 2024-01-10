f60c03e9b9381d95b8bd028e50359ac169a78177
Derek Mercedes <mercedes.derek@gmail.com>
2024-01-10 10:06:22 -0700

    tests passed

--- server/app.py
+++ server/app.py
@@ -16,6 +16,33 @@ db.init_app(app)
 
 api = Api(app)
 
+@app.route('/plants', methods=['GET', 'POST'])
+def plants():
+    if request.method == 'GET':
+        plants = Plant.query.all()
+        plant_response = []
+        for plant in plants:
+            plant_response.append(plant.to_dict())
+        
+        return make_response(plant_response, 200)
+    
+    if request.method == 'POST':
+        new_plant = Plant(
+            name = request.json.get('name'),
+            image = request.json.get('image'),
+            price = request.json.get('price')
+            )
+        db.session.add(new_plant)
+        db.session.commit()
+
+
+@app.route('/plants/<int:id>')
+def get_plant_by_id(id):
+    plant = Plant.query.filter(Plant.id == id).first().to_dict()
+    print(plant)
+    return make_response(plant, 200)
+
+
 class Plants(Resource):
     pass
 
